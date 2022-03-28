14410cec34bed17e5914e4c2c541e363a687ff92
rahulpathak-github <rpofficial.0001@gmail.com>
2022-03-28 15:55:46 +0530

    add right drawer

--- client/src/Components/DawerAppBar.js
+++ client/src/Components/DawerAppBar.js
@@ -17,13 +17,18 @@ import PersonAddAlt1Icon from "@mui/icons-material/PersonAddAlt1";
 import Avatar from "@mui/material/Avatar";
 import SearchBar from "./SearchBar";
 import Search from "@mui/icons-material/Search";
-// import DeleteIcon from "@mui/icons-material/Delete";
+import AddPost from "./AddPost";
 
 const DrawerAppBar = (props) => {
   const [open, setOpen] = React.useState(false);
+  const [discOpen, setDiscOpen] = React.useState(false);
 
   const handleDrawerToggle = () => {
     setOpen(!open);
+  };
+
+  const handleDiscoverDrawerToggle = () => {
+    setDiscOpen(!discOpen);
   };
 
   const drawer = (
@@ -33,6 +38,7 @@ const DrawerAppBar = (props) => {
           @username
         </Typography>
       </Toolbar>
+      <AddPost />
       <Divider />
       <List>
         {[
@@ -48,6 +54,60 @@ const DrawerAppBar = (props) => {
               <ListItemText primary={text.text} sx={{ float: "right" }} />
             </ListItem>
           </NavLink>
+        ))}
+      </List>
+    </div>
+  );
+
+  const discDrawer = (
+    <div>
+      <Toolbar>
+        <Search sx={{ paddingX: 2 }} />
+        <SearchBar />
+      </Toolbar>
+      <Divider />
+      <List>
+        {["Rahul Pathak", "Anand Amar", "Kushagra Gupta"].map((item, idx) => (
+          <ListItem
+            key={idx}
+            button
+            secondaryAction={
+              <IconButton>
+                <PersonAddAlt1Icon />
+              </IconButton>
+            }
+          >
+            <ListItemIcon>
+              <Avatar
+                src="https://imageio.forbes.com/specials-images/imageserve/5faad4255239c9448d6c7bcd/Best-Animal-Photos-Contest--Close-Up-Of-baby-monkey/960x0.jpg?fit=bounds&format=jpg&width=960"
+                sx={{ width: 50, height: 50 }}
+              />
+            </ListItemIcon>
+            <ListItemText
+              primary={
+                <React.Fragment>
+                  <Typography
+                    sx={{ display: "inline", fontSize: "18px" }}
+                    component="span"
+                    //   variant="body1"
+                  >
+                    {item}
+                  </Typography>
+                </React.Fragment>
+              }
+              secondary={
+                <React.Fragment>
+                  <Typography
+                    sx={{ display: "inline", fontSize: "6" }}
+                    component="span"
+                    variant="body2"
+                  >
+                    Follows You
+                  </Typography>
+                </React.Fragment>
+              }
+            />
+          </ListItem>
         ))}
       </List>
     </div>
@@ -80,6 +140,14 @@ const DrawerAppBar = (props) => {
           >
             Webathon
           </Typography>
+
+          <IconButton
+            edge="start"
+            sx={{ display: { md: "none" }, float: "right" }}
+            onClick={() => handleDiscoverDrawerToggle()}
+          >
+            <PersonAddAlt1Icon />
+          </IconButton>
         </Toolbar>
       </AppBar>
 
@@ -113,6 +181,36 @@ const DrawerAppBar = (props) => {
         {drawer}
       </Drawer>
 
+      <Drawer
+        open={discOpen}
+        anchor="right"
+        variant="temporary"
+        ModalProps={{ keepMounted: true }}
+        sx={{
+          display: { xs: "block", md: "none" },
+          "& .MuiDrawer-paper": {
+            boxSizing: "border-box",
+            width: props.drawerWidth,
+          },
+        }}
+        onClose={() => handleDiscoverDrawerToggle()}
+      >
+        {discDrawer}
+      </Drawer>
+
+      <Drawer
+        variant="permanent"
+        anchor="right"
+        sx={{
+          display: { xs: "none", md: "block" },
+          "& .MuiDrawer-paper": {
+            boxSizing: "border-box",
+            width: props.drawerWidth + 100,
+          },
+        }}
+      >
+        {discDrawer}
+      </Drawer>
       {/* </Box> */}
     </Box>
   );
