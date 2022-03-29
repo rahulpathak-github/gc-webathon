3da5a56d9b86660c429ad77e8a11db86e90f36e4
anand-2020 <62353883+anand-2020@users.noreply.github.com>
2022-03-29 21:55:18 +0530

    global error handling

---
+++ Controllers/errorController.js
@@ -0,0 +1,80 @@
+const AppError = require("./../Utils/appError");
+
+const duplicateFieldsDB = (err) => {
+  let value, key;
+  if (err.keyValue.handle) {
+    value = err.keyValue.handle;
+    key = "handle";
+  }
+  if (err.keyValue.email) {
+    value = err.keyValue.email;
+    key = "email";
+  }
+
+  const message = ` ${value} already exists. Please use another ${key}.`;
+  return new AppError(message, 400);
+};
+
+const validationErrorDB = (err) => {
+  const errors = Object.values(err.errors).map((el) => el.message);
+
+  const message = errors.join(". ");
+  return new AppError(message, 400);
+};
+
+const castErrorDB = (err) => {
+  const message = `Invalid ${err.path}: ${err.value}.`;
+  return new AppError(message, 400);
+};
+
+const handleJWTError = () =>
+  new AppError("Invalid token. Please login again", 401);
+
+const handleExpiredToken = () =>
+  new AppError("Token expired. Login in again", 401);
+
+const sendErrorDev = (err, res) => {
+  res.status(err.statusCode).json({
+    status: err.status,
+    message: err.message,
+    error: err,
+    stack: err.stack,
+  });
+};
+
+const sendErrorProd = (err, res) => {
+  // Operational, trusted error: send message to client
+  if (err.isOperational) {
+    res.status(err.statusCode).json({
+      status: err.status,
+      message: err.message,
+    });
+  } else {
+    console.error("ERROR:", err);
+
+    res.status(500).json({
+      status: "error",
+      message: "Something went wrong!",
+    });
+  }
+};
+
+module.exports = (err, req, res, next) => {
+  err.statusCode = err.statusCode || 500;
+  err.status = err.status || "error";
+
+  if (process.env.NODE_ENV === "development") {
+    sendErrorDev(err, res);
+  } else if (process.env.NODE_ENV === "production") {
+    let error = Object.assign(err);
+
+    if (error.name === "MongoError" && error.code === 11000)
+      error = duplicateFieldsDB(error);
+    else if (error.name === "ValidationError") error = validationErrorDB(error);
+    else if (error.name === "CastError") error = castErrorDB(error);
+    else if (error.name === "JsonWebTokenError") error = handleJWTError();
+    else if (error.name === "TokenExpiredError") error = handleExpiredToken();
+
+    sendErrorProd(error, res);
+  }
+};
--- app.js
+++ app.js
@@ -1,15 +1,27 @@
 const express = require("express");
+const cors = require("cors");
+const path = require("path");
 
 const authRouter = require("./Routers/authRouter");
 const userRouter = require("./Routers/userRouter");
 const postRouter = require("./Routers/postRouter");
-const commentRouter = require("./Routers/commentRouter");
 const imageRouter = require("./Routers/imageRouter");
+const commentRouter = require("./Routers/commentRouter");
 const AppError = require("./Utils/appError");
+const globalErrorHandler = require("./Controllers/errorController");
 
 const app = express();
 
+app.use(cors());
 app.use(express.json());
+
+// REACT BUILD for production
+if (process.env.NODE_ENV === "production") {
+  app.use(express.static(path.join(__dirname, "build")));
+  app.get("/*", (req, res) => {
+    res.sendFile(path.join(__dirname, "build", "index.html"));
+  });
+}
 
 //ROUTES
 app.use("/api/auth", authRouter);
@@ -21,5 +33,7 @@ app.use("/api/image", imageRouter);
 app.all("*", (req, res, next) => {
   next(new AppError(`Can't find ${req.originalUrl} on this server!`, 404));
 });
+
+app.use(globalErrorHandler);
 
 module.exports = app;
